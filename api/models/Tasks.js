e7d4e32d200ab766b9385faed0835c35de0d24da
fairyfade <77501073+fairyfade@users.noreply.github.com>
2023-03-15 15:00:31 -0400

    Star Size Change

--- api/models/Tasks.js
+++ api/models/Tasks.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     taskElement: {
       type: 'string',
-      defaultsTo: '<i class="fa fa-star fa-5x"></i>',
+      defaultsTo: '<i class="fa fa-star fa-3x"></i>',
     }
   }
 };
